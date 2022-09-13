4b08ea85a2fc922a6afe6a03a07d572dcd4c5459
2Miguel9 <108356947+2Miguel9@users.noreply.github.com>
2022-09-13 15:35:33 -0500

    portafolio

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Header(){
+    return(
+        <div className="header">
+            <p>Hola</p>
+        </div>
+    )
+}
+
+export default Header;
